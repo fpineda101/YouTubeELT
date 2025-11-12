02b843119be7ce50649417db7b8e973d336fd67b
fpineda191@hotmail.com <fpineda191@hotmail.com>
2025-11-11 20:22:02 -0800

    Fixed data checks

--- dags/main.py
+++ dags/main.py
@@ -9,6 +9,7 @@ except ImportError:
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
 
 from datawarehouse.dwh import staging_table, core_table
+from dataquality.soda import yt_elt_data_quality
 
 # define local timezone
 local_tz = pendulum.timezone("America/Los_Angeles")
@@ -68,10 +69,26 @@ with DAG(
     update_staging = staging_table()
     update_core = core_table()
 
-    #trigger_data_quality = TriggerDagRunOperator(
-    #    task_id="trigger_data_quality",
-    #    trigger_dag_id="data_quality",
-    #)
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
 
     # Define dependencies
-    update_staging >> update_core #>> trigger_data_quality
+    update_staging >> update_core >> trigger_data_quality
+
+    # DAG 3: data_quality
+with DAG(
+    dag_id="data_quality",
+    default_args=default_args,
+    description="DAG to check the data quality on both layers in the database",
+    catchup=False,
+    schedule=None,
+) as dag_quality:
+
+    # Define tasks
+    soda_validate_staging = yt_elt_data_quality(staging_schema)
+    soda_validate_core = yt_elt_data_quality(core_schema)
+
+    # Define dependencies
+    soda_validate_staging >> soda_validate_core
